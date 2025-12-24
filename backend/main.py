0e8a95ebb61c38d66829745ac066d05e904b35e8
zahid397 <zh05698@gmail.com>
2025-12-24 22:30:22 +0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import analyze
-import uvicorn
 
 app = FastAPI(title="Mobile Midwife API")
 
-# Enable CORS for React Frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Change to specific domain in production
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(analyze.router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
