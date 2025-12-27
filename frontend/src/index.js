26a272403ca7c2a5144d68b3bfb8dd0509429b6e
zahid397 <zh05698@gmail.com>
2025-12-27 16:47:31 +0600

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"; // নিশ্চিত করো যে এই লাইনটি এখানে আছে
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
